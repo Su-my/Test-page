c5b6586a77eb35548e04aff455b49c1e9b1efb66
苏明宇 <2021012207@secoder.net>
2024-09-14 15:46:56 +0800

    debug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    // 根据当前是否为dev环境，设置basePath
+    basePath: process.env.NODE_ENV === 'development' ? '' : '/Test-page',
+};
 
 export default nextConfig;
